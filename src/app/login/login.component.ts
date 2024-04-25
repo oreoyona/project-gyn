6b1f2cfc9eec7787ca71cab482e8c96d3ba3a1dd
Gloire <cortesgloire@gmail.com>
2024-04-26 01:35:26 +0200

    The loggin page has been built

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardMdImage, MatCardModule, } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, MatCardModule, FormsModule, ReactiveFormsModule, MatButtonModule],
+  imports: [MatInputModule, MatCardModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  showPassword = false; 
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 
   form: FormGroup = new FormGroup({
     username: new FormControl(''),
@@ -40,8 +46,6 @@ export class LoginComponent {
 
   @Output() submitEM = new EventEmitter();
 
-  
-  bgImage = "/assets/bg-1.webp";
   
     constructor(private route: Router){
 
